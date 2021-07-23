221c60b70fe7addb06d86b9d024d285bba410e21
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-07-23 01:07:20 +0100

    Grid cannot have negative X coordinate

--- TicTacToe/TicTacToeTests/TicTacToeGridTests.swift
+++ TicTacToe/TicTacToeTests/TicTacToeGridTests.swift
@@ -7,8 +7,26 @@
 
 import XCTest
 
+struct TicTacToeGrid {
+    
+    func fillSlot(at coordinates: Coordinates) -> TicTacToeGrid? {
+        return nil
+    }
+    
+}
+
+struct Coordinates {
+    let x, y: Int
+}
+
 class TicTacToeGridTests: XCTestCase {
 
-    
+    func test_fillSlot_xCoordinateCantBeNegative() {
+        let sut = TicTacToeGrid()
+        
+        let negativeXCoordinates = Coordinates(x: -1, y: 1)
+        
+        XCTAssertNil(sut.fillSlot(at: negativeXCoordinates))
+    }
 
 }
