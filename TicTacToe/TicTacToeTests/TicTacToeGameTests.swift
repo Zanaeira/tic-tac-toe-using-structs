7cc2e658b396771824df6311385f7c35e7e7113a
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-07-23 22:52:02 +0100

    playMove returns nil for out of bounds coordinates

--- TicTacToe/TicTacToeTests/TicTacToeGameTests.swift
+++ TicTacToe/TicTacToeTests/TicTacToeGameTests.swift
@@ -20,11 +20,17 @@ struct TicTacToeGame {
         nextPlayer = player
     }
     
-    func playMove(at coordinates: Coordinates) -> TicTacToeGameState {
+    func playMove(at coordinates: Coordinates) -> TicTacToeGameState? {
+        guard isWithinGridBounds(coordinates: coordinates) else { return nil }
+        
         let player: Player = nextPlayer == .player1 ? .player2 : .player1
         let game = TicTacToeGame(player: player)
         
         return .playing(game: game, nextPlayer: player)
+    }
+    
+    private func isWithinGridBounds(coordinates: Coordinates) -> Bool {
+        return (1...3 ~= coordinates.x) && (1...3 ~= coordinates.y)
     }
     
 }
@@ -39,7 +45,9 @@ class TicTacToeGameTests: XCTestCase {
         let sut = TicTacToeGame()
         
         let coordinates1 = Coordinates(x: 1, y: 1)
-        let newGameState = sut.playMove(at: coordinates1)
+        guard let newGameState = sut.playMove(at: coordinates1) else {
+            return XCTFail("playMove returned nil")
+        }
         guard case let TicTacToeGameState.playing(game, nextPlayer) = newGameState else {
             return XCTFail("playMove did not return playing state with game and nextPlayer")
         }
@@ -47,11 +55,36 @@ class TicTacToeGameTests: XCTestCase {
         XCTAssertEqual(nextPlayer, Player.player2)
         
         let coordinates2 = Coordinates(x: 1, y: 2)
-        guard case let TicTacToeGameState.playing(_, nextPlayer) = game.playMove(at: coordinates2) else {
+        guard let newGameState = game.playMove(at: coordinates2) else {
+            return XCTFail("playMove returned nil")
+        }
+        guard case let TicTacToeGameState.playing(_, nextPlayer) = newGameState else {
             return XCTFail("playMove did not return playing state with game and nextPlayer")
         }
         
         XCTAssertEqual(nextPlayer, Player.player1)
+    }
+    
+    func test_playMove_returnsNilOnOutOfBoundsCoordinates() {
+        let sut = TicTacToeGame()
+        
+        let negativeXCoordinate = Coordinates(x: -1, y: 1)
+        XCTAssertNil(sut.playMove(at: negativeXCoordinate), "Negative x-coordinate should result in nil for playMove")
+        
+        let negativeYCoordinate = Coordinates(x: 1, y: -1)
+        XCTAssertNil(sut.playMove(at: negativeYCoordinate), "Negative y-coordinate should result in nil for playMove")
+        
+        let zeroXCoordinate = Coordinates(x: 0, y: 1)
+        XCTAssertNil(sut.playMove(at: zeroXCoordinate), "Zero x-coordinate should result in nil for playMove")
+        
+        let zeroYCoordinate = Coordinates(x: 1, y: 0)
+        XCTAssertNil(sut.playMove(at: zeroYCoordinate), "Zero y-coordinate should result in nil for playMove")
+        
+        let greaterThanThreeXCoordinate = Coordinates(x: 4, y: 1)
+        XCTAssertNil(sut.playMove(at: greaterThanThreeXCoordinate), "Greater than 3 x-coordinate should result in nil for playMove")
+        
+        let greaterThanThreeYCoordinate = Coordinates(x: 1, y: 4)
+        XCTAssertNil(sut.playMove(at: greaterThanThreeYCoordinate), "Greater than 3 y-coordinate should result in nil for playMove")
     }
     
 }
